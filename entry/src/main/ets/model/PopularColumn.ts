13bfc05606773286ebebd08799e231681e047520
yong.zhang23.o <yong.zhang23.o@nio.com>
2024-08-13 17:35:08 +0800

    提交
    Description:
    Module:
    Type: BUG REFACTOR NEW
    Effect:

---
+++ entry/src/main/ets/model/PopularColumn.ts
@@ -0,0 +1,39 @@
+
+
+export class PopularColumn {
+    adminAdd: boolean;
+    apkLink: string;
+    audit: number;
+    author: string;
+    canEdit: boolean;
+    chapterId: number;
+    chapterName: string;
+    collect: boolean;
+    courseId: number;
+    desc: string;
+    descMd: string;
+    envelopePic: string;
+    fresh: boolean;
+    host: string;
+    id: number;
+    isAdminAdd: boolean;
+    link: string;
+    niceDate: string;
+    niceShareDate: string;
+    origin: string;
+    prefix: string;
+    projectLink: string;
+    publishTime: number;
+    realSuperChapterId: number;
+    selfVisible: number;
+    shareDate: number;
+    shareUser: string;
+    superChapterId: number;
+    superChapterName: string;
+    tags: any[];
+    title: string;
+    type: number;
+    userId: number;
+    visible: number;
+    zan: number;
+}
---
+++ entry/src/main/ets/model/PopularWenDa.ts
@@ -0,0 +1,13 @@
+
+
+export class PopularWenDa {
+    chapterId: number;
+    chapterName: string;
+    columnId: number;
+    id: number;
+    name: string;
+    subChapterId: number;
+    subChapterName: string;
+    url: string;
+    userId: number;
+}
